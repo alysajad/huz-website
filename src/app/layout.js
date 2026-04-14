6fcc2574295aa20feffd9882177af615eea8af97
alysajad <huxi1314k@gmail.com>
2026-04-14 23:51:13 +0530

    added tag manager

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import { Cormorant_Garamond, Inter } from "next/font/google";
+import Script from "next/script";
 import TopNavBar from "@/components/TopNavBar";
 import Footer from "@/components/Footer";
 import "./globals.css";
@@ -24,9 +25,33 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className="scroll-smooth">
       <head>
+        {/* Google Tag Manager */}
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-P2QXH8BV');`,
+          }}
+        />
+        {/* End Google Tag Manager */}
         <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght,FILL@100..700,0..1&display=swap" rel="stylesheet" />
       </head>
       <body className={`${cormorant.variable} ${inter.variable} bg-surface text-on-surface font-sans selection:bg-primary-container selection:text-on-primary-container min-h-screen flex flex-col`}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-P2QXH8BV"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+        
         <TopNavBar />
         <main className="flex-grow">
           {children}
